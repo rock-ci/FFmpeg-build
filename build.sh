1be3944bdb122cb839bf8fc3a65b690a9f1e93a6
Hugh Cole-Baker <sigmaris@gmail.com>
2022-10-23 11:06:26 +0100

    change to correct dir before building

--- build.sh
+++ build.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 set -euo pipefail
+SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
+
+source "${SCRIPT_DIR}/lib/versions.sh"
 
 echo "***********************"
 echo "** Building packages **"
 echo "***********************"
 
+cd "ffmpeg-${UPSTREAM_VER}"
 dpkg-buildpackage -b -us -uc --jobs=auto
 
 echo "****************"
