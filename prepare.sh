47ff2e768d285742103d227269567b578dfc0573
Hugh Cole-Baker <sigmaris@gmail.com>
2023-08-05 16:08:16 +0100

    build with special patches for Raspberry Pi

--- prepare.sh
+++ prepare.sh
@@ -4,11 +4,14 @@ SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
 
 source "${SCRIPT_DIR}/lib/versions.sh"
 
+: "${PATCH_DIR:=patches}"
+
 echo ""
 echo "Upstream version: $UPSTREAM_VER"
 echo "Debian revision:  $DEBIAN_REV"
 echo "Our revision      $OUR_REV"
 echo "Debian arch:      $DEB_ARCH"
+echo "Patch dir:        ${SCRIPT_DIR}/${PATCH_DIR}"
 echo ""
 
 
@@ -27,7 +30,7 @@ echo "************************"
 cat << EOF | tee debian/changelog.new
 ffmpeg (10:${UPSTREAM_VER}-${OUR_REV}) unstable; urgency=medium
 
-  * Add v4l2-request hardware acceleration based on LibreELEC
+  * Add v4l2 hardware acceleration based on LibreELEC
 
  -- Hugh Cole-Baker <sigmaris@gmail.com>  $(date '+%a, %d %b %Y %H:%M:%S %z')
 
@@ -39,7 +42,7 @@ echo "***************************"
 echo "** Applying ffmpeg patches **"
 echo "***************************"
 
-for patchfile in "${SCRIPT_DIR}"/patches/*.patch
+for patchfile in "${SCRIPT_DIR}/${PATCH_DIR}"/*.patch
 do
     echo "* Applying $patchfile ..."
     patch -p1 < "$patchfile"
